8e393a268ebbf7048f2cd6f43ef54dbd84e98f90
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-07-15 11:24:05 +0300

    Initial commit

---
+++ src/test/java/acme/guessgame/guessgame/GuessGameApplicationTests.java
@@ -0,0 +1,16 @@
+package acme.guessgame.guessgame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuessGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
