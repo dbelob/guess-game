c05b318245d7a69b0d1c60c82551c723786a4e64
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-07-22 10:57:06 +0300

    Change filenames and names

--- guess-game-server/src/main/java/acme/guess/domain/Question.java
+++ guess-game-server/src/main/java/acme/guess/domain/Question.java
@@ -2,8 +2,8 @@ package acme.guess.domain;
 
 public class Question {
     private long id;
-    private String name;
     private String fileName;
+    private String name;
 
     public long getId() {
         return id;
@@ -13,20 +13,20 @@ public class Question {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getFileName() {
         return fileName;
     }
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
