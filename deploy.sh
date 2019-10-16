008f3895194a9a07e2e07d1d950b2be39a8cf9fb
inponomarev <iponomarev@mail.ru>
2019-10-16 13:28:30 +0300

    fix pk permissions

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 openssl enc -d -aes-256-cbc -in deploy_rsa.enc -out deploy_rsa -pass env:DECYPHER_KEY
+chmod 400 deploy_rsa
 scp -i deploy_rsa -o "StrictHostKeyChecking no" guess-game-server/target/guess-game.war root@jugspeakers.online:/opt/guess/guess-game-${TRAVIS_BUILD_NUMBER}.war
