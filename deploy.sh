330281ed929aef90408d2678afe0f2926fc5c5a0
inponomarev <iponomarev@mail.ru>
2019-10-16 14:04:49 +0300

    fix encoding issue

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-openssl enc -d -aes-256-cbc -md md5 -in deploy_rsa.enc -out deploy_rsa -pass env:DECYPHER_KEY
+openssl enc -a -d -aes-256-cbc -in deploy_rsa.enc -out deploy_rsa -pass env:DECYPHER_KEY
 chmod 400 deploy_rsa
 scp -i deploy_rsa -o "StrictHostKeyChecking no" guess-game-server/target/guess-game.war root@jugspeakers.online:/opt/guess/guess-game-${TRAVIS_BUILD_NUMBER}.war
 rm deploy_rsa
