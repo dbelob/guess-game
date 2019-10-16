0f149d2db3e655055c86819143fe2a8cb5028cb7
inponomarev <iponomarev@mail.ru>
2019-10-16 13:03:20 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openssl enc -d -aes-256-cbc -in deploy_rsa.enc -out deploy_rsa -pass ${DECYPHER_KEY}
-scp -i deploy_rsa guess-game-server/target/guess-game.war root@jugspeakers.online:/opt/guess/guess-game-${TRAVIS_BUILD_NUMBER}.war
+openssl enc -d -aes-256-cbc -in deploy_rsa.enc -out deploy_rsa -pass env:DECYPHER_KEY
+scp -i deploy_rsa -o "StrictHostKeyChecking no" guess-game-server/target/guess-game.war root@jugspeakers.online:/opt/guess/guess-game-${TRAVIS_BUILD_NUMBER}.war
