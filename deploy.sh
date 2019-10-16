4bc1b10289d14ea20eb6eac54ea2ac8ca1b4a16d
inponomarev <iponomarev@mail.ru>
2019-10-16 14:31:14 +0300

    skip cleanup and force rm

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -x
 openssl enc -a -d -aes-256-cbc -in deploy_rsa.enc -out deploy_rsa -pass env:DECYPHER_KEY
 chmod 400 deploy_rsa
 scp -i deploy_rsa -o "StrictHostKeyChecking no" guess-game-server/target/guess-game.war root@jugspeakers.online:/opt/guess/guess-game-${TRAVIS_BUILD_NUMBER}.war
-rm deploy_rsa
+rm -f deploy_rsa
