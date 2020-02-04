827737fa4709fd2de53a195610690c9ec372bacc
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-02-04 20:38:48 +0300

    Try extend emitter

--- guess-game-server/src/main/java/guess/util/yaml/CustomEmitter.java
+++ guess-game-server/src/main/java/guess/util/yaml/CustomEmitter.java
@@ -12,4 +12,33 @@ public class CustomEmitter {
     public CustomEmitter(Writer stream, DumperOptions opts) {
 //        super(stream, opts);
     }
+
+    private DumperOptions.ScalarStyle chooseScalarStyle() {
+/*        ScalarEvent ev = (ScalarEvent) event;
+        if (analysis == null) {
+            analysis = analyzeScalar(ev.getValue());
+        }
+        if (!ev.isPlain() && ev.getScalarStyle() == DumperOptions.ScalarStyle.DOUBLE_QUOTED || this.canonical) {
+            return DumperOptions.ScalarStyle.DOUBLE_QUOTED;
+        }
+        if (ev.isPlain() && ev.getImplicit().canOmitTagInPlainScalar()) {
+            if (!(simpleKeyContext && (analysis.empty || analysis.multiline))
+                    && ((flowLevel != 0 && analysis.allowFlowPlain) || (flowLevel == 0 && analysis.allowBlockPlain))) {
+                return null;
+            }
+        }
+        if (!ev.isPlain() && (ev.getScalarStyle() == DumperOptions.ScalarStyle.LITERAL || ev.getScalarStyle() == DumperOptions.ScalarStyle.FOLDED)) {
+            if (flowLevel == 0 && !simpleKeyContext && analysis.allowBlock) {
+//                return ev.getScalarStyle();
+                return DumperOptions.ScalarStyle.DOUBLE_QUOTED;
+            }
+        }
+        if (ev.isPlain() || ev.getScalarStyle() == DumperOptions.ScalarStyle.SINGLE_QUOTED) {
+            if (analysis.allowSingleQuoted && !(simpleKeyContext && analysis.multiline)) {
+//                return DumperOptions.ScalarStyle.SINGLE_QUOTED;
+                return DumperOptions.ScalarStyle.DOUBLE_QUOTED;
+            }
+        } */
+        return DumperOptions.ScalarStyle.DOUBLE_QUOTED;
+    }
 }
