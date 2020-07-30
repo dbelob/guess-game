ddba3dc257c151bda2cb181d1158ecd6bb97d008
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-07-30 14:27:16 +0300

    Fix warnings

--- guess-game-server/src/main/java/guess/domain/answer/Answer.java
+++ guess-game-server/src/main/java/guess/domain/answer/Answer.java
@@ -2,10 +2,12 @@ package guess.domain.answer;
 
 import guess.domain.Identifiable;
 
+import java.io.Serializable;
+
 /**
  * Answer.
  */
-public class Answer extends Identifiable {
+public class Answer extends Identifiable implements Serializable {
     public Answer(long id) {
         super(id);
     }
--- guess-game-server/src/main/java/guess/domain/question/Question.java
+++ guess-game-server/src/main/java/guess/domain/question/Question.java
@@ -2,10 +2,12 @@ package guess.domain.question;
 
 import guess.domain.Identifiable;
 
+import java.io.Serializable;
+
 /**
  * Question.
  */
-public abstract class Question extends Identifiable {
+public abstract class Question extends Identifiable implements Serializable {
     public Question(long id) {
         super(id);
     }
