586b23d5235e7f65444127d1d70440f34d410dac
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-08-11 15:24:20 +0300

    Add tests

--- guess-game-server/src/test/java/guess/util/ConferenceDataLoaderTest.java
+++ guess-game-server/src/test/java/guess/util/ConferenceDataLoaderTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.time.LocalTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
@@ -59,6 +60,58 @@ public class ConferenceDataLoaderTest {
         @MethodSource("data")
         void deleteOpeningAndClosingTalks(List<Talk> talks, List<Talk> expected) {
             assertEquals(expected, ConferenceDataLoader.deleteOpeningAndClosingTalks(talks));
+        }
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DisplayName("deleteTalkDuplicates method tests")
+    class DeleteTalkDuplicatesTest {
+        private Stream<Arguments> data() {
+            Talk talk0 = new Talk();
+            talk0.setId(0);
+            talk0.setName(List.of(new LocaleItem("ru", "name0")));
+            talk0.setTalkDay(1L);
+            talk0.setTrack(1L);
+            talk0.setTrackTime(LocalTime.of(10, 0));
+
+            Talk talk1 = new Talk();
+            talk1.setId(1);
+            talk1.setName(List.of(new LocaleItem("ru", "name0")));
+            talk1.setTalkDay(2L);
+
+            Talk talk2 = new Talk();
+            talk2.setId(2);
+            talk2.setName(List.of(new LocaleItem("ru", "name0")));
+            talk2.setTalkDay(1L);
+            talk2.setTrack(2L);
+
+            Talk talk3 = new Talk();
+            talk3.setId(3);
+            talk3.setName(List.of(new LocaleItem("ru", "name0")));
+            talk3.setTalkDay(1L);
+            talk3.setTrack(1L);
+            talk3.setTrackTime(LocalTime.of(10, 30));
+
+            Talk talk4 = new Talk();
+            talk4.setId(4);
+            talk4.setName(List.of(new LocaleItem("ru", "name4")));
+
+            return Stream.of(
+                    arguments(Collections.emptyList(), Collections.emptyList()),
+                    arguments(List.of(talk0), List.of(talk0)),
+                    arguments(List.of(talk1, talk0), List.of(talk0)),
+                    arguments(List.of(talk1, talk2, talk0), List.of(talk0)),
+                    arguments(List.of(talk1, talk2, talk3, talk0), List.of(talk0)),
+                    arguments(List.of(talk0, talk4), List.of(talk0, talk4)),
+                    arguments(List.of(talk1, talk2, talk3, talk0, talk4), List.of(talk0, talk4))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("data")
+        void deleteTalkDuplicates(List<Talk> talks, List<Talk> expected) {
+            assertEquals(expected, ConferenceDataLoader.deleteTalkDuplicates(talks));
         }
     }
 }
