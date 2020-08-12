d1879f2d3235b6404b555d0682494e204e25e0a5
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-08-12 16:40:17 +0300

    Add tests

--- guess-game-server/src/test/java/guess/util/ConferenceDataLoaderTest.java
+++ guess-game-server/src/test/java/guess/util/ConferenceDataLoaderTest.java
@@ -95,16 +95,26 @@ public class ConferenceDataLoaderTest {
 
             Talk talk4 = new Talk();
             talk4.setId(4);
-            talk4.setName(List.of(new LocaleItem("ru", "name4")));
+            talk4.setName(List.of(new LocaleItem("ru", "name0")));
+            talk4.setTalkDay(1L);
+            talk4.setTrack(1L);
+            talk4.setTrackTime(LocalTime.of(11, 0));
+
+            Talk talk5 = new Talk();
+            talk5.setId(5);
+            talk5.setName(List.of(new LocaleItem("ru", "name5")));
 
             return Stream.of(
                     arguments(Collections.emptyList(), Collections.emptyList()),
                     arguments(List.of(talk0), List.of(talk0)),
                     arguments(List.of(talk1, talk0), List.of(talk0)),
+                    arguments(List.of(talk0, talk1), List.of(talk0)),
                     arguments(List.of(talk1, talk2, talk0), List.of(talk0)),
+                    arguments(List.of(talk1, talk0, talk2), List.of(talk0)),
                     arguments(List.of(talk1, talk2, talk3, talk0), List.of(talk0)),
-                    arguments(List.of(talk0, talk4), List.of(talk0, talk4)),
-                    arguments(List.of(talk1, talk2, talk3, talk0, talk4), List.of(talk0, talk4))
+                    arguments(List.of(talk0, talk5), List.of(talk0, talk5)),
+                    arguments(List.of(talk1, talk2, talk3, talk0, talk4), List.of(talk0)),
+                    arguments(List.of(talk1, talk2, talk3, talk0, talk5), List.of(talk0, talk5))
             );
         }
 
