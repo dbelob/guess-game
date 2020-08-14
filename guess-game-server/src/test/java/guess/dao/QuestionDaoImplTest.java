89ef45a4084178054b4481ce89d508dcc7910baa
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-08-14 15:29:25 +0300

    Migrate from JUnit 4 to JUnit 5

--- guess-game-server/src/test/java/guess/dao/QuestionDaoImplTest.java
+++ guess-game-server/src/test/java/guess/dao/QuestionDaoImplTest.java
@@ -3,6 +3,7 @@ package guess.dao;
 import guess.dao.exception.SpeakerDuplicatedException;
 import guess.domain.question.QuestionSet;
 import guess.domain.question.SpeakerQuestion;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("QuestionDao class tests")
 class QuestionDaoImplTest {
     @Test
     void questionSetsImagesExistence() throws IOException, SpeakerDuplicatedException {
