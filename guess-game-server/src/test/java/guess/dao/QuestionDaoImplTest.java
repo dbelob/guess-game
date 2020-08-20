85854cb7c061297b108579a74e5588f26de82ff2
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-08-20 14:04:54 +0300

    Add tests

--- guess-game-server/src/test/java/guess/dao/QuestionDaoImplTest.java
+++ guess-game-server/src/test/java/guess/dao/QuestionDaoImplTest.java
@@ -5,17 +5,12 @@ import guess.domain.Conference;
 import guess.domain.question.QuestionSet;
 import guess.domain.question.SpeakerQuestion;
 import guess.domain.question.TalkQuestion;
-import guess.domain.source.Event;
-import guess.domain.source.EventType;
-import guess.domain.source.Speaker;
-import guess.domain.source.Talk;
+import guess.domain.source.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -34,26 +29,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class QuestionDaoImplTest {
     @TestConfiguration
     static class QuestionDaoImplTestContextConfiguration {
-        @MockBean
-        private EventTypeDao eventTypeDao;
-
-        @MockBean
-        private EventDao eventDao;
-
         @Bean
         public QuestionDao questionDao() {
-            Mockito.when(eventTypeDao.getEventTypeById(0)).thenReturn(eventType0);
-            Mockito.when(eventDao.getEvents()).thenReturn(List.of(event0, event1, event2));
+            SourceInformation sourceInformation = new SourceInformation(
+                    Collections.emptyList(),
+                    List.of(eventType0, eventType1, eventType2),
+                    List.of(event0, event1, event2, event3),
+                    List.of(speaker0, speaker1, speaker2, speaker3),
+                    List.of(talk0, talk1, talk2));
+            SourceDao sourceDao = new SourceDaoImpl(sourceInformation);
 
-            return new QuestionDaoImpl(eventTypeDao, eventDao);
+            return new QuestionDaoImpl(sourceDao, sourceDao);
         }
     }
-
-    @Autowired
-    private EventTypeDao eventTypeDao;
-
-    @Autowired
-    private EventDao eventDao;
 
     @Autowired
     private QuestionDao questionDao;
@@ -68,6 +56,7 @@ class QuestionDaoImplTest {
     private static final Event event0;
     private static final Event event1;
     private static final Event event2;
+    private static final Event event3;
     private static final EventType eventType0;
     private static final EventType eventType1;
     private static final EventType eventType2;
@@ -121,12 +110,17 @@ class QuestionDaoImplTest {
         event2.setTalkIds(List.of(2L));
         event2.setTalks(List.of(talk2));
 
+        event3 = new Event();
+        event3.setId(3);
+
         eventType0 = new EventType();
         eventType0.setId(0);
-        eventType0.setEvents(List.of(event0));
         eventType0.setConference(Conference.JPOINT);
+        eventType0.setEvents(List.of(event0, event3));
         event0.setEventTypeId(0);
         event0.setEventType(eventType0);
+        event3.setEventTypeId(0);
+        event3.setEventType(eventType0);
 
         eventType1 = new EventType();
         eventType1.setId(1);
@@ -174,6 +168,12 @@ class QuestionDaoImplTest {
                                         new TalkQuestion(List.of(speaker2, speaker3), talk2)
                                 ),
                                 List.of(new SpeakerQuestion(speaker2))
+                        ),
+                        new QuestionSet(
+                                event3,
+                                Collections.emptyList(),
+                                Collections.emptyList(),
+                                Collections.emptyList()
                         )
                 ),
                 questionDao.readQuestionSets());
@@ -195,13 +195,78 @@ class QuestionDaoImplTest {
                         Collections.emptyList()
                 )
         );
-//        assertEquals(
-//                Collections.emptyList(),
-//                questionDao.getSubQuestionSets(
-//                        Collections.singletonList(0L),
-//                        Collections.singletonList(0L)
-//                )
-//        );
+        assertEquals(
+                List.of(new QuestionSet(
+                        event0,
+                        List.of(
+                                new SpeakerQuestion(speaker0)
+                        ),
+                        List.of(
+                                new TalkQuestion(List.of(speaker0), talk0)
+                        ),
+                        Collections.emptyList()
+                )),
+                questionDao.getSubQuestionSets(
+                        Collections.singletonList(0L),
+                        Collections.singletonList(0L)
+                )
+        );
+        assertEquals(
+                List.of(new QuestionSet(
+                        event3,
+                        Collections.emptyList(),
+                        Collections.emptyList(),
+                        Collections.emptyList()
+                )),
+                questionDao.getSubQuestionSets(
+                        Collections.singletonList(0L),
+                        Collections.singletonList(3L)
+                )
+        );
+        assertEquals(
+                List.of(new QuestionSet(
+                        event1,
+                        List.of(
+                                new SpeakerQuestion(speaker1)
+                        ),
+                        List.of(
+                                new TalkQuestion(List.of(speaker1), talk1)
+                        ),
+                        List.of(new SpeakerQuestion(speaker1))
+                )),
+                questionDao.getSubQuestionSets(
+                        Collections.singletonList(1L),
+                        Collections.singletonList(1L)
+                )
+        );
+        assertEquals(
+                List.of(new QuestionSet(
+                                event1,
+                                List.of(
+                                        new SpeakerQuestion(speaker1)
+                                ),
+                                List.of(
+                                        new TalkQuestion(List.of(speaker1), talk1)
+                                ),
+                                List.of(new SpeakerQuestion(speaker1))
+                        ),
+                        new QuestionSet(
+                                event2,
+                                List.of(
+                                        new SpeakerQuestion(speaker2),
+                                        new SpeakerQuestion(speaker3)
+                                ),
+                                List.of(
+                                        new TalkQuestion(List.of(speaker2, speaker3), talk2)
+                                ),
+                                List.of(new SpeakerQuestion(speaker2))
+                        )
+                ),
+                questionDao.getSubQuestionSets(
+                        List.of(1L, 2L),
+                        Collections.emptyList()
+                )
+        );
     }
 
     @Test
