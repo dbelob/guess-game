f54e37f60daa5c28d5845c88e474d3d3ec1e9e4f
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-08-26 12:55:59 +0300

    Support inherited fields

--- guess-game-server/src/main/java/guess/util/yaml/PropertyMatcher.java
+++ guess-game-server/src/main/java/guess/util/yaml/PropertyMatcher.java
@@ -1,6 +1,10 @@
 package guess.util.yaml;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PropertyMatcher {
     private final Class<?> clazz;
@@ -11,9 +15,25 @@ public class PropertyMatcher {
         this.propertyNames = propertyNames;
 
         // Check fields existence
+        List<String> fieldNames = getFields(clazz).stream()
+                .map(Field::getName)
+                .collect(Collectors.toList());
+
         for (String propertyName : propertyNames) {
-            clazz.getDeclaredField(propertyName);
+            if (!fieldNames.contains(propertyName)) {
+                throw new NoSuchFieldException(propertyName);
+            }
         }
+    }
+
+    List<Field> getFields(Class<?> clazz) {
+        List<Field> result = new ArrayList<>(Arrays.asList(clazz.getDeclaredFields()));
+
+        if (clazz.getSuperclass() != null) {
+            result.addAll(getFields(clazz.getSuperclass()));
+        }
+
+        return result;
     }
 
     public Class<?> getClazz() {
