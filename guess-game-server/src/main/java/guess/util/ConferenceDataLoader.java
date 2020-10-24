92dc4a7fdb3584a1c28b754e73d49135df218cea
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-10-24 19:02:04 +0300

    Change Heisenbug 2020 Msc information

--- guess-game-server/src/main/java/guess/util/ConferenceDataLoader.java
+++ guess-game-server/src/main/java/guess/util/ConferenceDataLoader.java
@@ -1334,9 +1334,9 @@ public class ConferenceDataLoader {
 //                LoadSettings.knownSpeakerIdsMap(Map.of(new NameCompany("Oleg Anastasyev", "Odnoklassniki"), 124L)));
 //        loadTalksSpeakersEvent(Conference.SPTDC, LocalDate.of(2020, 7, 6), "2020-msk-sptdc",
 //                LoadSettings.invalidTalksSet(Set.of("Doctoral workshop", "Title will be announced soon")));
-        loadTalksSpeakersEvent(Conference.TECH_TRAIN, LocalDate.of(2020, 10, 24), "2020techtrainautumn",
-                LoadSettings.ignoreDemoStage(false));
-//        loadTalksSpeakersEvent(Conference.HEISENBUG, LocalDate.of(2020, 11, 4), "2020msk");
+//        loadTalksSpeakersEvent(Conference.TECH_TRAIN, LocalDate.of(2020, 10, 24), "2020techtrainautumn");
+//        loadTalksSpeakersEvent(Conference.HEISENBUG, LocalDate.of(2020, 11, 4), "2020msk",
+//                LoadSettings.ignoreDemoStage(false));
 //        loadTalksSpeakersEvent(Conference.MOBIUS, LocalDate.of(2020, 11, 11), "2020msk");
 //        loadTalksSpeakersEvent(Conference.CPP_RUSSIA, LocalDate.of(2020, 11, 11), "2020spbcpp");
 //        loadTalksSpeakersEvent(Conference.JOKER, LocalDate.of(2020, 11, 25), "2020joker");
