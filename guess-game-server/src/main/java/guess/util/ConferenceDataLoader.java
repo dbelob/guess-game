c50567b559761f20c1bbd9677c20096a16a3b83d
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-12-02 11:43:09 +0300

    Change C++ Russia 2020 Spb information

--- guess-game-server/src/main/java/guess/util/ConferenceDataLoader.java
+++ guess-game-server/src/main/java/guess/util/ConferenceDataLoader.java
@@ -1336,7 +1336,10 @@ public class ConferenceDataLoader {
 //                LoadSettings.invalidTalksSet(Set.of("Doctoral workshop", "Title will be announced soon")));
 //        loadTalksSpeakersEvent(Conference.TECH_TRAIN, LocalDate.of(2020, 10, 24), "2020techtrainautumn");
 //        loadTalksSpeakersEvent(Conference.CPP_RUSSIA, LocalDate.of(2020, 11, 11), "2020spbcpp",
-//                LoadSettings.invalidTalksSet(Set.of("Lightning talks", "C++ Russia 2020 Piter Virtual Afterparty")));
+//                new LoadSettings(
+//                        Collections.emptyMap(),
+//                        Set.of("Lightning talks", "C++ Russia 2020 Piter Virtual Afterparty"),
+//                        false));
 //        loadTalksSpeakersEvent(Conference.HEISENBUG, LocalDate.of(2020, 11, 4), "2020msk",
 //                new LoadSettings(
 //                        Collections.emptyMap(),
