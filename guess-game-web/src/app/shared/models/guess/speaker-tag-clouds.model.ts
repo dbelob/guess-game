13639bb2658e8bf73200d6c32fb30455a87724d6
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-03-14 20:19:07 +0300

    Change data type

--- guess-game-web/src/app/shared/models/guess/speaker-tag-clouds.model.ts
+++ guess-game-web/src/app/shared/models/guess/speaker-tag-clouds.model.ts
@@ -4,10 +4,10 @@ import { SpeakerPair } from '../result/speaker-pair.model';
 export class SpeakerTagClouds extends QuestionAnswers {
   constructor(
     public speaker?: SpeakerPair,
-    public image0?: any,
-    public image1?: any,
-    public image2?: any,
-    public image3?: any
+    public image0?: string,
+    public image1?: string,
+    public image2?: string,
+    public image3?: string
   ) {
     super();
   }
