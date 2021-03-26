22ef27d056b9fbe2073fc383bfe68cbdc16b2158
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-03-26 11:14:21 +0300

    Add tests

--- guess-game-server/src/test/java/guess/util/tagcloud/TagCloudUtilsTest.java
+++ guess-game-server/src/test/java/guess/util/tagcloud/TagCloudUtilsTest.java
@@ -1,7 +1,12 @@
 package guess.util.tagcloud;
 
+import com.kennycason.kumo.WordCloud;
 import com.kennycason.kumo.WordFrequency;
+import com.kennycason.kumo.bg.Background;
+import com.kennycason.kumo.font.scale.FontScalar;
 import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.palette.ColorPalette;
+import guess.dao.exception.WrapperRuntimeException;
 import guess.domain.Language;
 import guess.domain.source.LocaleItem;
 import guess.domain.source.Talk;
@@ -19,13 +24,15 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.*;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @DisplayName("TagCloudUtils class tests")
@@ -100,39 +107,33 @@ class TagCloudUtilsTest {
     class LoadStopWordsTest {
         private Stream<Arguments> data() {
             return Stream.of(
-                    arguments(true),
-                    arguments(false)
+                    arguments(true, List.of("first", "second", "third")),
+                    arguments(false, List.of("first", "second", "third"))
             );
         }
 
         @ParameterizedTest
         @MethodSource("data")
-        void loadStopWords(boolean withoutException) {
+        void loadStopWords(boolean withoutException, List<String> lines) {
             new MockUp<IOUtils>() {
                 @Mock
                 List<String> readLines(InputStream input) throws IOException {
                     if (withoutException) {
-                        return Collections.emptyList();
+                        return lines;
                     } else {
                         throw new IOException("Exception");
                     }
                 }
             };
 
-            assertDoesNotThrow(TagCloudUtils::loadStopWords);
-        }
-    }
+            Set<String> actual = TagCloudUtils.loadStopWords();
 
-    @Test
-    void loadStopWords() {
-        new MockUp<IOUtils>() {
-            @Mock
-            List<String> readLines(InputStream input) throws IOException {
-                return Collections.emptyList();
+            if (withoutException) {
+                assertFalse(actual.isEmpty());
+            } else {
+                assertTrue(actual.isEmpty());
             }
-        };
-
-        assertDoesNotThrow(TagCloudUtils::loadStopWords);
+        }
     }
 
     @Test
@@ -232,5 +233,103 @@ class TagCloudUtilsTest {
         List<Map<Language, List<SerializedWordFrequency>>> languageWordFrequenciesMapList = List.of(languageWordFrequenciesMap0, languageWordFrequenciesMap1);
 
         assertDoesNotThrow(() -> TagCloudUtils.mergeWordFrequenciesMaps(languageWordFrequenciesMapList));
+    }
+
+    @Test
+    void createImage() {
+        new MockUp<WordCloud>() {
+            @Mock
+            void setBackgroundColor(final Color backgroundColor) {
+                // Nothing
+            }
+
+            @Mock
+            void setPadding(final int padding) {
+                // Nothing
+            }
+
+            @Mock
+            void setBackground(final Background background) {
+                // Nothing
+            }
+
+            @Mock
+            void setColorPalette(final ColorPalette colorPalette) {
+                // Nothing
+            }
+
+            @Mock
+            void setFontScalar(final FontScalar fontScalar) {
+                // Nothing
+            }
+
+            @Mock
+            void build(final List<WordFrequency> wordFrequencies) {
+                // Nothing
+            }
+
+            @Mock
+            BufferedImage getBufferedImage() {
+                return new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+            }
+        };
+
+        SerializedWordFrequency wordFrequency0 = new SerializedWordFrequency("first", 42);
+        SerializedWordFrequency wordFrequency1 = new SerializedWordFrequency("second", 41);
+        SerializedWordFrequency wordFrequency2 = new SerializedWordFrequency("third", 40);
+        List<SerializedWordFrequency> wordFrequencies = List.of(wordFrequency0, wordFrequency1, wordFrequency2);
+
+        assertDoesNotThrow(() -> TagCloudUtils.createImage(wordFrequencies));
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DisplayName("createLanguageImageMap method tests")
+    class CreateLanguageImageMapTest {
+        private Stream<Arguments> data() {
+            SerializedWordFrequency wordFrequency0 = new SerializedWordFrequency("first", 42);
+            SerializedWordFrequency wordFrequency1 = new SerializedWordFrequency("second", 41);
+            SerializedWordFrequency wordFrequency2 = new SerializedWordFrequency("third", 40);
+            SerializedWordFrequency wordFrequency3 = new SerializedWordFrequency("fourth", 39);
+            SerializedWordFrequency wordFrequency4 = new SerializedWordFrequency("fifth", 38);
+            List<SerializedWordFrequency> wordFrequencies0 = List.of(wordFrequency0, wordFrequency1, wordFrequency2);
+            List<SerializedWordFrequency> wordFrequencies1 = List.of(wordFrequency3, wordFrequency4);
+            Map<Language, List<SerializedWordFrequency>> languageWordFrequenciesMap = Map.of(
+                    Language.ENGLISH, wordFrequencies0,
+                    Language.RUSSIAN, wordFrequencies1);
+
+            return Stream.of(
+                    arguments(languageWordFrequenciesMap, true),
+                    arguments(languageWordFrequenciesMap, false)
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("data")
+        void createLanguageImageMap(Map<Language, List<SerializedWordFrequency>> languageWordFrequenciesMap, boolean withoutException) {
+            new MockUp<TagCloudUtils>() {
+                @Mock
+                Map<Language, byte[]> createLanguageImageMap(
+                        Invocation invocation,
+                        Map<Language, List<SerializedWordFrequency>> languageWordFrequenciesMap) {
+                    return invocation.proceed(languageWordFrequenciesMap);
+                }
+
+                @Mock
+                byte[] createImage(List<SerializedWordFrequency> wordFrequencies) throws IOException {
+                    if (withoutException) {
+                        return new byte[]{};
+                    } else {
+                        throw new IOException("Exception");
+                    }
+                }
+            };
+
+            if (withoutException) {
+                assertDoesNotThrow(() -> TagCloudUtils.createLanguageImageMap(languageWordFrequenciesMap));
+            } else {
+                assertThrows(WrapperRuntimeException.class, () -> TagCloudUtils.createLanguageImageMap(languageWordFrequenciesMap));
+            }
+        }
     }
 }
