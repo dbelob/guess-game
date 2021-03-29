f6089bdad479a59dda05a90463e7a0ac3fe8669e
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-03-29 20:55:35 +0300

    Add tests

--- guess-game-server/src/main/java/guess/util/yaml/EventComparator.java
+++ guess-game-server/src/main/java/guess/util/yaml/EventComparator.java
@@ -6,6 +6,7 @@ import guess.domain.source.Talk;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -25,55 +26,62 @@ public class EventComparator implements Comparator<Event> {
             if (event2 == null) {
                 return 1;
             } else {
-                LocalDate eventStartDate1 = event1.getStartDate();
-                LocalDate eventStartDate2 = event2.getStartDate();
+                return compareStartDate(event1, event2);
+            }
+        }
+    }
 
-                // Compare start date
-                if (eventStartDate1 == null) {
-                    if (eventStartDate2 == null) {
-                        return 0;
-                    } else {
-                        return -1;
-                    }
+    static int compareStartDate(Event event1, Event event2) {
+        LocalDate eventStartDate1 = event1.getStartDate();
+        LocalDate eventStartDate2 = event2.getStartDate();
+
+        // Compare start date
+        if (eventStartDate1 == null) {
+            if (eventStartDate2 == null) {
+                return 0;
+            } else {
+                return -1;
+            }
+        } else {
+            if (eventStartDate2 == null) {
+                return 1;
+            } else {
+                // Compare track time
+                if (eventStartDate1.isEqual(eventStartDate2)) {
+                    return compareTrackTime(event1, event2);
                 } else {
-                    if (eventStartDate2 == null) {
-                        return 1;
-                    } else {
-                        // Compare track time
-                        if (eventStartDate1.isEqual(eventStartDate2)) {
-                            Optional<LocalTime> eventTrackTime1 = event1.getTalks().stream()
-                                    .filter(t -> (t.getTalkDay() != null) && (t.getTrackTime() != null))
-                                    .sorted(Comparator.comparing(Talk::getTalkDay).thenComparing(Talk::getTrackTime))
-                                    .map(Talk::getTrackTime)
-                                    .findFirst();
+                    return eventStartDate1.isBefore(eventStartDate2) ? -1 : 1;
+                }
+            }
+        }
+    }
 
-                            Optional<LocalTime> eventTrackTime2 = event2.getTalks().stream()
-                                    .filter(t -> (t.getTalkDay() != null) && (t.getTrackTime() != null))
-                                    .sorted(Comparator.comparing(Talk::getTalkDay).thenComparing(Talk::getTrackTime))
-                                    .map(Talk::getTrackTime)
-                                    .findFirst();
+    static Optional<LocalTime> getFirstTrackTime(List<Talk> talks) {
+        return talks.stream()
+                .filter(t -> (t.getTalkDay() != null) && (t.getTrackTime() != null))
+                .sorted(Comparator.comparing(Talk::getTalkDay).thenComparing(Talk::getTrackTime))
+                .map(Talk::getTrackTime)
+                .findFirst();
+    }
 
-                            if (eventTrackTime1.isEmpty()) {
-                                if (eventTrackTime2.isEmpty()) {
-                                    return 0;
-                                } else {
-                                    return -1;
-                                }
-                            } else {
-                                if (eventTrackTime2.isEmpty()) {
-                                    return 1;
-                                } else {
-                                    if (eventTrackTime1.get().equals(eventTrackTime2.get())) {
-                                        return 0;
-                                    } else {
-                                        return eventTrackTime1.get().isBefore(eventTrackTime2.get()) ? -1 : 1;
-                                    }
-                                }
-                            }
-                        } else {
-                            return eventStartDate1.isBefore(eventStartDate2) ? -1 : 1;
-                        }
-                    }
+    static int compareTrackTime(Event event1, Event event2) {
+        Optional<LocalTime> eventTrackTime1 = getFirstTrackTime(event1.getTalks());
+        Optional<LocalTime> eventTrackTime2 = getFirstTrackTime(event2.getTalks());
+
+        if (eventTrackTime1.isEmpty()) {
+            if (eventTrackTime2.isEmpty()) {
+                return 0;
+            } else {
+                return -1;
+            }
+        } else {
+            if (eventTrackTime2.isEmpty()) {
+                return 1;
+            } else {
+                if (eventTrackTime1.get().equals(eventTrackTime2.get())) {
+                    return 0;
+                } else {
+                    return eventTrackTime1.get().isBefore(eventTrackTime2.get()) ? -1 : 1;
                 }
             }
         }
--- guess-game-server/src/test/java/guess/util/yaml/EventComparatorTest.java
+++ guess-game-server/src/test/java/guess/util/yaml/EventComparatorTest.java
@@ -2,8 +2,12 @@ package guess.util.yaml;
 
 import guess.domain.source.Event;
 import guess.domain.source.Talk;
+import mockit.Invocation;
+import mockit.Mock;
+import mockit.MockUp;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -12,9 +16,11 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class EventComparatorTest {
@@ -23,6 +29,39 @@ class EventComparatorTest {
     @DisplayName("compare method tests")
     class CompareTest {
         private Stream<Arguments> data() {
+            final int COMPARE_START_DATE_RESULT = 42;
+
+            Event event0 = new Event();
+            Event event1 = new Event();
+
+            return Stream.of(
+                    arguments(null, null, COMPARE_START_DATE_RESULT, 0),
+                    arguments(null, event0, COMPARE_START_DATE_RESULT, -1),
+                    arguments(event0, null, COMPARE_START_DATE_RESULT, 1),
+                    arguments(event0, event1, COMPARE_START_DATE_RESULT, COMPARE_START_DATE_RESULT)
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("data")
+        void compare(Event event1, Event event2, int compareStartDateResult, int expected) {
+            new MockUp<EventComparator>() {
+                @Mock
+                int compareStartDate(Event event1, Event event2) {
+                    return compareStartDateResult;
+                }
+            };
+
+            assertEquals(expected, new EventComparator().compare(event1, event2));
+        }
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DisplayName("compareStartDate method tests")
+    class CompareStartDateTest {
+        private Stream<Arguments> data() {
+            final int COMPARE_TRACK_TIME_RESULT = 42;
             LocalDate START_DATE0 = LocalDate.of(2021, 3, 29);
             LocalDate START_DATE1 = LocalDate.of(2021, 3, 30);
             LocalDate START_DATE2 = LocalDate.of(2021, 3, 28);
@@ -43,48 +82,118 @@ class EventComparatorTest {
             Event event5 = new Event();
             event5.setStartDate(START_DATE0);
 
-            Talk talk0 = new Talk();
-            talk0.setTalkDay(1L);
-            talk0.setTrackTime(LocalTime.of(10, 30));
-
-            Event event6 = new Event();
-            event6.setStartDate(START_DATE0);
-            event6.setTalks(List.of(talk0));
-
-            Event event7 = new Event();
-            event7.setStartDate(START_DATE0);
-            event7.setTalks(List.of(talk0));
-
-            Talk talk1 = new Talk();
-            talk1.setTalkDay(1L);
-            talk1.setTrackTime(LocalTime.of(10, 45));
-
-            Event event8 = new Event();
-            event8.setStartDate(START_DATE0);
-            event8.setTalks(List.of(talk1));
-
             return Stream.of(
-                    arguments(null, null, 0),
-                    arguments(null, event0, -1),
-                    arguments(event0, null, 1),
-                    arguments(event0, event1, 0),
-                    arguments(event1, event2, -1),
-                    arguments(event2, event1, 1),
-                    arguments(event2, event3, -1),
-                    arguments(event2, event4, 1),
-                    arguments(event2, event5, 0),
-                    arguments(event2, event6, -1),
-                    arguments(event6, event2, 1),
-                    arguments(event6, event7, 0),
-                    arguments(event6, event8, -1),
-                    arguments(event8, event6, 1)
+                    arguments(event0, event1, COMPARE_TRACK_TIME_RESULT, 0),
+                    arguments(event1, event2, COMPARE_TRACK_TIME_RESULT, -1),
+                    arguments(event2, event1, COMPARE_TRACK_TIME_RESULT, 1),
+                    arguments(event2, event3, COMPARE_TRACK_TIME_RESULT, -1),
+                    arguments(event2, event4, COMPARE_TRACK_TIME_RESULT, 1),
+                    arguments(event2, event5, COMPARE_TRACK_TIME_RESULT, COMPARE_TRACK_TIME_RESULT)
             );
         }
 
         @ParameterizedTest
         @MethodSource("data")
-        void compare(Event event1, Event event2, int expected) {
-            assertEquals(expected, new EventComparator().compare(event1, event2));
+        void compareStartDate(Event event1, Event event2, int compareTrackTimeResult, int expected) {
+            new MockUp<EventComparator>() {
+                @Mock
+                int compareStartDate(Invocation invocation, Event event1, Event event2) {
+                    return invocation.proceed(event1, event2);
+                }
+
+                @Mock
+                int compareTrackTime(Event event1, Event event2) {
+                    return compareTrackTimeResult;
+                }
+            };
+
+            assertEquals(expected, EventComparator.compareStartDate(event1, event2));
+        }
+    }
+
+    @Test
+    void getFirstTrackTime() {
+        Talk talk0 = new Talk();
+
+        Talk talk1 = new Talk();
+        talk1.setTalkDay(1L);
+
+        Talk talk2 = new Talk();
+        talk2.setTrackTime(LocalTime.of(10, 0));
+
+        Talk talk3 = new Talk();
+        talk3.setTalkDay(1L);
+        talk3.setTrackTime(LocalTime.of(10, 30));
+
+        Talk talk4 = new Talk();
+        talk4.setTalkDay(1L);
+        talk4.setTrackTime(LocalTime.of(10, 45));
+
+        Talk talk5 = new Talk();
+        talk5.setTalkDay(2L);
+        talk5.setTrackTime(LocalTime.of(9, 0));
+
+        List<Talk> talks = List.of(talk0, talk1, talk2, talk3, talk4);
+
+        LocalTime expected = LocalTime.of(10, 30);
+        Optional<LocalTime> actual = EventComparator.getFirstTrackTime(talks);
+
+        assertTrue(actual.isPresent());
+        assertEquals(expected, actual.get());
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DisplayName("compareTrackTime method tests")
+    class CompareTrackTimeTest {
+        private Stream<Arguments> data() {
+            Event event0 = new Event();
+
+            Event event1 = new Event();
+
+            Talk talk0 = new Talk();
+            talk0.setTalkDay(1L);
+            talk0.setTrackTime(LocalTime.of(10, 30));
+
+            Event event2 = new Event();
+            event2.setTalks(List.of(talk0));
+
+            Event event3 = new Event();
+            event3.setTalks(List.of(talk0));
+
+            Talk talk1 = new Talk();
+            talk1.setTalkDay(1L);
+            talk1.setTrackTime(LocalTime.of(10, 45));
+
+            Event event4 = new Event();
+            event4.setTalks(List.of(talk1));
+
+            return Stream.of(
+                    arguments(event0, event1, 0),
+                    arguments(event0, event2, -1),
+                    arguments(event2, event0, 1),
+                    arguments(event2, event3, 0),
+                    arguments(event2, event4, -1),
+                    arguments(event4, event2, 1)
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("data")
+        void compareTrackTime(Event event1, Event event2, int expected) {
+            new MockUp<EventComparator>() {
+                @Mock
+                int compareTrackTime(Invocation invocation, Event event1, Event event2) {
+                    return invocation.proceed(event1, event2);
+                }
+
+                @Mock
+                Optional<LocalTime> getFirstTrackTime(List<Talk> talks) {
+                    return talks.isEmpty() ? Optional.empty() : Optional.of(talks.get(0).getTrackTime());
+                }
+            };
+
+            assertEquals(expected, EventComparator.compareTrackTime(event1, event2));
         }
     }
 }
