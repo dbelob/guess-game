98c4b526d967904dd85c527f54ee24f782e736cf
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-12-06 20:44:51 +0300

    Fix warnings

--- guess-game-server/src/main/java/guess/service/SpeakerServiceImpl.java
+++ guess-game-server/src/main/java/guess/service/SpeakerServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Speaker service implementation.
