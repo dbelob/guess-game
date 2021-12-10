3dd5b944ac78b0f87c5a727fdcf72a264b856f91
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-12-10 17:13:59 +0300

    Fix Sonar warnings

--- guess-game-server/src/main/java/guess/domain/source/Company.java
+++ guess-game-server/src/main/java/guess/domain/source/Company.java
@@ -30,6 +30,16 @@ public class Company extends Nameable {
     }
 
     @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
     public String toString() {
         return "Company{" +
                 "id=" + getId() +
