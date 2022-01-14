2d36bae2082c1738dd442137178f935f79cd1072
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-01-14 13:39:25 +0300

    Add abstract class

--- guess-game-web/src/app/modules/information/companies/companies-list.component.ts
+++ guess-game-web/src/app/modules/information/companies/companies-list.component.ts
@@ -1,30 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { CompanySearchResult } from '../../../shared/models/company/company-search-result.model';
+import { EntitiesListComponent } from '../entity-list.component';
 import { CompanyService } from '../../../shared/services/company.service';
 
 @Component({
   selector: 'app-companies-list',
   templateUrl: './companies-list.component.html'
 })
-export class CompaniesListComponent implements OnInit {
+export class CompaniesListComponent extends EntitiesListComponent implements OnInit {
   private readonly DEFAULT_IS_DIGIT = false;
-  private readonly DEFAULT_LETTER = 'A';
-  private readonly RUSSIAN_LANG = 'ru';
   public readonly DIGIT_BUTTON_TEXT = '0-9';
 
-  public enLetters: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  public ruLetters: string[] = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р',
-    'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я'];
-
   public isDigit = this.DEFAULT_IS_DIGIT;
-  public selectedLetter = this.DEFAULT_LETTER;
 
   public companies: CompanySearchResult[] = [];
-  public paginatorFirst = 0;
 
-  constructor(private companyService: CompanyService, public translateService: TranslateService) {
+  constructor(private companyService: CompanyService, translateService: TranslateService) {
+    super(translateService);
   }
 
   ngOnInit(): void {
@@ -34,10 +27,6 @@ export class CompaniesListComponent implements OnInit {
   loadCompanies(isDigit: boolean, letter: string) {
     // TODO: implement
     console.log('isDigit: ' + ', letter: ' + letter);
-  }
-
-  onLanguageChange() {
-    this.changeLetter(this.DEFAULT_LETTER);
   }
 
   isCurrentLetter(letter: string) {
@@ -56,10 +45,6 @@ export class CompaniesListComponent implements OnInit {
   changeDigit() {
     this.isDigit = true;
     this.selectedLetter = null;
-  }
-
-  isRuLettersVisible(): boolean {
-    return (this.translateService.currentLang === this.RUSSIAN_LANG);
   }
 
   isCompaniesListVisible() {
