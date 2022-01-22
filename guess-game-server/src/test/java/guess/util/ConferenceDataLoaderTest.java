72d97f68407f6e1a288b58fa63212c7b4411d5ba
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-01-22 21:31:51 +0300

    Add tests

--- guess-game-server/src/test/java/guess/util/ConferenceDataLoaderTest.java
+++ guess-game-server/src/test/java/guess/util/ConferenceDataLoaderTest.java
@@ -2924,6 +2924,11 @@ class ConferenceDataLoaderTest {
             Company company4 = new Company(4, List.of(new LocaleItem(Language.ENGLISH.getCode(), "Name4")), "https://site2.com");
             Company company5 = new Company(5, List.of(new LocaleItem(Language.ENGLISH.getCode(), "Name5")), "https://site2.com");
 
+            Speaker speaker0 = new Speaker();
+            speaker0.setId(0);
+            speaker0.setName(List.of(new LocaleItem(Language.ENGLISH.getCode(), "Name0")));
+            speaker0.setCompanies(List.of(company0));
+
             mockedStatic.when(YamlUtils::readSourceInformation)
                     .thenReturn(new SourceInformation(Collections.emptyList(), Collections.emptyList(),
                             Collections.emptyList(), Collections.emptyList(),
@@ -2931,7 +2936,7 @@ class ConferenceDataLoaderTest {
                                     List.of(company0, company1, company2, company3, company4, company5),
                                     Collections.emptyList(),
                                     Collections.emptyList(),
-                                    Collections.emptyList()
+                                    List.of(speaker0)
                             ),
                             Collections.emptyList()
                     ));
