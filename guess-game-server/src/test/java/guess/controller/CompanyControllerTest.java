4dbc74b2058dad7d71847cc6b27f0ddc671dec89
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-01-23 23:27:57 +0300

    Add tests

--- guess-game-server/src/test/java/guess/controller/CompanyControllerTest.java
+++ guess-game-server/src/test/java/guess/controller/CompanyControllerTest.java
@@ -6,11 +6,18 @@ import guess.domain.source.LocaleItem;
 import guess.domain.source.Speaker;
 import guess.domain.statistics.company.CompanySearchResult;
 import guess.dto.common.SelectedEntitiesDto;
+import guess.dto.company.CompanySearchResultDto;
 import guess.service.CompanyService;
 import guess.service.LocaleService;
 import guess.service.SpeakerService;
+import guess.util.SearchUtils;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.mockito.internal.verification.VerificationModeFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +27,14 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -140,6 +151,7 @@ class CompanyControllerTest {
         MockHttpSession httpSession = new MockHttpSession();
 
         Company company0 = new Company(0, List.of(new LocaleItem(Language.ENGLISH.getCode(), "Company0")));
+        Company company1 = new Company(1, List.of(new LocaleItem(Language.ENGLISH.getCode(), "Company1")));
 
         Speaker speaker0 = new Speaker();
         speaker0.setId(0);
@@ -149,7 +161,7 @@ class CompanyControllerTest {
         Speaker speaker1 = new Speaker();
         speaker1.setId(1);
         speaker1.setName(List.of(new LocaleItem(Language.ENGLISH.getCode(), "Name1")));
-        speaker1.setCompanies(List.of(company0));
+        speaker1.setCompanies(List.of(company0, company1));
 
         given(companyService.getCompanyById(0)).willReturn(company0);
         given(localeService.getLanguage(httpSession)).willReturn(Language.ENGLISH);
@@ -203,5 +215,84 @@ class CompanyControllerTest {
         Mockito.verify(localeService, VerificationModeFactory.times(1)).getLanguage(httpSession);
         Mockito.verify(companyService, VerificationModeFactory.times(1)).getCompaniesByFirstLetter(IS_DIGIT, FIRST_LETTER, LANGUAGE);
         Mockito.verify(companyService, VerificationModeFactory.times(1)).getCompanySearchResults(Mockito.anyList());
+    }
+
+    @Test
+    void getCompanies() throws Exception {
+        final String NAME = "name";
+
+        MockHttpSession httpSession = new MockHttpSession();
+
+        Company company0 = new Company();
+        company0.setId(0);
+        company0.setName(List.of(new LocaleItem(Language.ENGLISH.getCode(), "Name0")));
+
+        Company company1 = new Company();
+        company1.setId(1);
+        company1.setName(List.of(new LocaleItem(Language.ENGLISH.getCode(), "Name1")));
+
+        CompanySearchResult companySearchResult0 = new CompanySearchResult(company0, 1, 0, 0);
+        CompanySearchResult companySearchResult1 = new CompanySearchResult(company1, 1, 0, 0);
+
+        given(localeService.getLanguage(httpSession)).willReturn(Language.ENGLISH);
+        given(companyService.getCompanies(NAME)).willReturn(List.of(company1, company0));
+        given(companyService.getCompanySearchResults(Mockito.anyList())).willReturn(List.of(companySearchResult1, companySearchResult0));
+
+        mvc.perform(get("/api/company/companies")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("name", NAME)
+                        .session(httpSession))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(0)))
+                .andExpect(jsonPath("$[1].id", is(1)));
+        Mockito.verify(localeService, VerificationModeFactory.times(1)).getLanguage(httpSession);
+        Mockito.verify(companyService, VerificationModeFactory.times(1)).getCompanies(NAME);
+        Mockito.verify(companyService, VerificationModeFactory.times(1)).getCompanySearchResults(Mockito.anyList());
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DisplayName("calculateAndConvertToDtoAndSort method tests")
+    class CalculateAndConvertToDtoAndSortTest {
+        private Stream<Arguments> data() {
+            Company company0 = new Company();
+            company0.setId(0);
+            company0.setName(List.of(new LocaleItem(Language.ENGLISH.getCode(), "Name0")));
+
+            Company company1 = new Company();
+            company1.setId(1);
+            company1.setName(List.of(new LocaleItem(Language.ENGLISH.getCode(), "Name1")));
+
+            Comparator<CompanySearchResultDto> comparatorByName = Comparator.comparing(
+                    CompanySearchResultDto::name, String.CASE_INSENSITIVE_ORDER);
+            Comparator<CompanySearchResultDto> comparatorByNameWithFirstAlphaNumeric = Comparator.comparing(
+                    c -> SearchUtils.getSubStringWithFirstAlphaNumeric(c.name()), String.CASE_INSENSITIVE_ORDER);
+
+            CompanySearchResult companySearchResult0 = new CompanySearchResult(company0, 1, 0, 0);
+            CompanySearchResult companySearchResult1 = new CompanySearchResult(company1, 1, 0, 0);
+
+            CompanySearchResultDto companySearchResultDto0 = new CompanySearchResultDto(0, "Name0", 1, 0, 0);
+            CompanySearchResultDto companySearchResultDto1 = new CompanySearchResultDto(1, "Name1", 1, 0, 0);
+
+            return Stream.of(
+                    arguments(List.of(company0, company1), Language.ENGLISH, comparatorByName,
+                            List.of(companySearchResult1, companySearchResult0), List.of(companySearchResultDto0, companySearchResultDto1)),
+                    arguments(List.of(company0, company1), Language.ENGLISH, comparatorByNameWithFirstAlphaNumeric,
+                            List.of(companySearchResult1, companySearchResult0), List.of(companySearchResultDto0, companySearchResultDto1))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("data")
+        void calculateAndConvertToDtoAndSort(List<Company> companies, Language language,
+                                             Comparator<CompanySearchResultDto> comparator,
+                                             List<CompanySearchResult> companySearchResults, List<CompanySearchResultDto> expected) {
+            given(companyService.getCompanySearchResults(Mockito.anyList())).willReturn(companySearchResults);
+
+            CompanyController companyController = new CompanyController(companyService, speakerService, localeService);
+
+            assertEquals(expected, companyController.calculateAndConvertToDtoAndSort(companies, language, comparator));
+        }
     }
 }
