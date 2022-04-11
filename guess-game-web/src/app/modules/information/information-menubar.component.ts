985459b614458714db4a375af2eb5b7c3f0d9bc1
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-04-11 13:48:52 +0300

    Change information menubar

--- guess-game-web/src/app/modules/information/information-menubar.component.ts
+++ guess-game-web/src/app/modules/information/information-menubar.component.ts
@@ -1,16 +1,50 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-information-menubar',
   templateUrl: './information-menubar.component.html'
 })
 export class InformationMenubarComponent implements OnInit {
+  private readonly EVENT_TYPES_TITLE_KEY = 'eventTypes.title';
+  private readonly EVENTS_TITLE_KEY = 'events.title';
+  private readonly TALKS_TITLE_KEY = 'talks.title';
+  private readonly SPEAKERS_TITLE_KEY = 'speakers.title';
+  private readonly COMPANIES_TITLE_KEY = 'companies.title';
+  private readonly STATISTICS_TITLE_KEY = 'statistics.title';
+
+  private readonly KEYS = [this.EVENT_TYPES_TITLE_KEY, this.EVENTS_TITLE_KEY, this.TALKS_TITLE_KEY,
+    this.SPEAKERS_TITLE_KEY, this.COMPANIES_TITLE_KEY, this.STATISTICS_TITLE_KEY];
+
   @Input() private type: string;
 
-  constructor() {
+  public items: MenuItem[] = [];
+
+  constructor(public translateService: TranslateService) {
   }
 
   ngOnInit(): void {
+    this.initMenuItems();
+
+    this.translateService.onLangChange
+      .subscribe(() => {
+        this.initMenuItems();
+      });
+  }
+
+  initMenuItems() {
+    this.translateService.get(this.KEYS)
+      .subscribe(labels => {
+        this.items = [
+          {label: labels[this.EVENT_TYPES_TITLE_KEY], routerLink: '/information/event-types'},
+          {label: labels[this.EVENTS_TITLE_KEY], routerLink: '/information/events'},
+          {label: labels[this.TALKS_TITLE_KEY], routerLink: '/information/talks'},
+          {label: labels[this.SPEAKERS_TITLE_KEY], routerLink: '/information/speakers'},
+          {label: labels[this.COMPANIES_TITLE_KEY], routerLink: '/information/companies'},
+          {label: labels[this.STATISTICS_TITLE_KEY], routerLink: '/information/statistics'}
+        ];
+      });
   }
 
   isEventTypes(): boolean {
--- guess-game-web/src/app/modules/information/information.module.ts
+++ guess-game-web/src/app/modules/information/information.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { MenubarModule } from 'primeng/menubar';
+import { TabViewModule } from 'primeng/tabview';
 import { InformationMenubarComponent } from './information-menubar.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { InformationMenubarComponent } from './information-menubar.component';
   ],
   imports: [
     CommonModule,
+    MenubarModule,
     RouterModule,
+    TabViewModule,
     TranslateModule
   ],
   exports: [
